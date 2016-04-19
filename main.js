34a1759cfa54004c8989eddf2c541b6343567cf8
Corinne Winthrop <corinne.winthrop@gmail.com>
2016-04-18 20:01:29 -0600

    Finish 3 problems of hw

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
-
-	$('#hover').on('hover', function(){
-		$(this).css('background-color','red');
+	$("#hover").on({
+	    mouseenter: function () {
+			$(this).css('background-color','red').css('transition','300ms');
+	    },
+	    mouseleave: function () {
+			$(this).css('background-color','blue').css('transition','300ms');
+	    },
+	    click: function() {
+		    $(this).toggleClass('green');
+	    }
 	});
-
+	$('.hideMe').on('click', function(){
+		var $this = $(this);
+		$this.hide();
+	});
+	$('.changeColor').on('click', function(){
+		$('li:nth-child(3)').css('color','blue');
+	});
 });
